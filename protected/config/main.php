a3f40aad96305e27c9ee1a8570905ef4358c6322
Nikolay Rudakov <nnrudakov@gmail.com>
2014-03-06 00:58:44 +0400

    Подготовка окружения.

--- protected/config/main.php
+++ protected/config/main.php
@@ -5,86 +5,76 @@
 
 // This is the main Web application configuration. Any writable
 // CWebApplication properties can be configured here.
-return array(
-	'basePath'=>dirname(__FILE__).DIRECTORY_SEPARATOR.'..',
-	'name'=>'My Web Application',
-
-	// preloading 'log' component
-	'preload'=>array('log'),
-
-	// autoloading model and component classes
-	'import'=>array(
-		'application.models.*',
-		'application.components.*',
-	),
-
-	'modules'=>array(
-		// uncomment the following to enable the Gii tool
-		/*
-		'gii'=>array(
-			'class'=>'system.gii.GiiModule',
-			'password'=>'Enter Your Password Here',
-			// If removed, Gii defaults to localhost only. Edit carefully to taste.
-			'ipFilters'=>array('127.0.0.1','::1'),
-		),
-		*/
-	),
-
-	// application components
-	'components'=>array(
-		'user'=>array(
-			// enable cookie-based authentication
-			'allowAutoLogin'=>true,
-		),
-		// uncomment the following to enable URLs in path-format
-		/*
-		'urlManager'=>array(
-			'urlFormat'=>'path',
-			'rules'=>array(
-				'<controller:\w+>/<id:\d+>'=>'<controller>/view',
-				'<controller:\w+>/<action:\w+>/<id:\d+>'=>'<controller>/<action>',
-				'<controller:\w+>/<action:\w+>'=>'<controller>/<action>',
-			),
-		),
-		*/
-		'db'=>array(
-			'connectionString' => 'sqlite:'.dirname(__FILE__).'/../data/testdrive.db',
-		),
-		// uncomment the following to use a MySQL database
-		/*
-		'db'=>array(
-			'connectionString' => 'mysql:host=localhost;dbname=testdrive',
-			'emulatePrepare' => true,
-			'username' => 'root',
-			'password' => '',
-			'charset' => 'utf8',
-		),
-		*/
-		'errorHandler'=>array(
-			// use 'site/error' action to display errors
-			'errorAction'=>'site/error',
-		),
-		'log'=>array(
-			'class'=>'CLogRouter',
-			'routes'=>array(
-				array(
-					'class'=>'CFileLogRoute',
-					'levels'=>'error, warning',
-				),
-				// uncomment the following to show log messages on web pages
-				/*
-				array(
-					'class'=>'CWebLogRoute',
-				),
-				*/
-			),
-		),
-	),
-
-	// application-level parameters that can be accessed
-	// using Yii::app()->params['paramName']
-	'params'=>array(
-		// this is used in contact page
-		'adminEmail'=>'webmaster@example.com',
-	),
-);
+return [
+    'basePath'   => __DIR__ . DIRECTORY_SEPARATOR . '..',
+    'name'       => 'Converter',
+    // preloading 'log' component
+    'preload'    => ['log'],
+    // autoloading model and component classes
+    'import'     => [
+        'application.models.*',
+        'application.components.*',
+    ],
+    'modules'    => [
+        // uncomment the following to enable the Gii tool
+        'gii'=>[
+            'class' => 'system.gii.GiiModule',
+            'password' => '1',
+            // If removed, Gii defaults to localhost only. Edit carefully to taste.
+            'ipFilters'=>['127.0.0.1','::1'],
+        ],
+    ],
+    // application components
+    'components' => [
+        'user'         => [
+            // enable cookie-based authentication
+            'allowAutoLogin' => true,
+        ],
+        // uncomment the following to enable URLs in path-format
+        'urlManager'=>[
+            'showScriptName' => false,
+            'urlFormat' => 'path',
+            'rules' => [
+                '<controller:\w+>/<id:\d+>'=>'<controller>/view',
+                '<controller:\w+>/<action:\w+>/<id:\d+>'=>'<controller>/<action>',
+                '<controller:\w+>/<action:\w+>'=>'<controller>/<action>',
+            ],
+        ],
+        /*'db'           => [
+            'connectionString' => 'sqlite:' . dirname(__FILE__) . '/../data/testdrive.db',
+        ],*/
+        // uncomment the following to use a MySQL database
+        'db'=>[
+            'connectionString' => 'pgsql:host=localhost;port=5432;dbname=krasnodar',
+            'emulatePrepare' => true,
+            'username' => 'postgres',
+            'password' => '1234',
+            'charset' => 'utf8',
+        ],
+        'errorHandler' => [
+            // use 'site/error' action to display errors
+            'errorAction' => 'site/error',
+        ],
+        'log'          => [
+            'class'  => 'CLogRouter',
+            'routes' => [
+                [
+                    'class'  => 'CFileLogRoute',
+                    'levels' => 'error, warning',
+                ],
+                // uncomment the following to show log messages on web pages
+                /*
+                [
+                    'class'=>'CWebLogRoute',
+                ],
+                */
+            ],
+        ],
+    ],
+    // application-level parameters that can be accessed
+    // using Yii::app()->params['paramName']
+    'params'     => [
+        // this is used in contact page
+        'adminEmail' => 'nnrudakov@gmail.com',
+    ],
+];
---
+++ protected/gii/model/templates/converter/model.php
@@ -0,0 +1,155 @@
+<?php
+/**
+ * This is the template for generating the model class of a specified table.
+ * - $this: the ModelCode object
+ * - $tableName: the table name for this class (prefix is already removed if necessary)
+ * - $modelClass: the model class name
+ * - $columns: list of table columns (name=>CDbColumnSchema)
+ * - $labels: list of attribute labels (name=>label)
+ * - $rules: list of validation rules
+ * - $relations: list of relations (name=>relation declaration)
+ */
+?>
+<?php echo "<?php\n"; ?>
+
+/**
+ * Модель таблицы "<?php echo $tableName; ?>".
+ *
+ * Доступные поля таблицы "<?php echo $tableName; ?>":
+<?php foreach($columns as $column): ?>
+<?$labels[$column->name] = $column->comment;?>
+ * @property <?php echo $column->type.' $'.$column->name.' '.$labels[$column->name].".\n"; ?>
+<?php endforeach; ?>
+<?php if(!empty($relations)): ?>
+ *
+ * Доступные отношения:
+<?php foreach($relations as $name=>$relation): ?>
+ * @property <?php
+    if (preg_match("~^array\(self::([^,]+), '([^']+)', '([^']+)'\)$~", $relation, $matches))
+    {
+        $relationType = $matches[1];
+        $relationModel = $matches[2];
+
+        switch($relationType){
+            case 'HAS_ONE':
+                echo $relationModel.' $'.$name."\n";
+            break;
+            case 'BELONGS_TO':
+                echo $relationModel.' $'.$name."\n";
+            break;
+            case 'HAS_MANY':
+                echo $relationModel.'[] $'.$name."\n";
+            break;
+            case 'MANY_MANY':
+                echo $relationModel.'[] $'.$name."\n";
+            break;
+            default:
+                echo 'mixed $'.$name."\n";
+        }
+	}
+    ?>
+<?php endforeach; ?>
+<?php endif; ?>
+ *
+ * @package Converter
+ * @subpackage <?php echo strtolower($modelClass)."\n"; ?>
+ * @author rudnik <nnrudakov@gmail.com>
+ * @copyright 2014
+ */
+class <?php echo $modelClass; ?> extends <?php echo $this->baseClass."\n"; ?>
+{
+	/**
+	 * @return string Таблица модели
+	 */
+	public function tableName()
+    {
+		return '<?php echo $tableName; ?>';
+	}
+
+	/**
+	 * @return array Правила валидации.
+	 */
+	public function rules()
+    {
+		return [
+<?php foreach($rules as $rule): ?>
+			<?php echo str_replace(['array(', '),'], ['[', '],'], $rule.",\n"); ?>
+<?php endforeach; ?>
+			['<?php echo implode(', ', array_keys($columns)); ?>', 'safe', 'on'=>'search'],
+		];
+	}
+
+	/**
+	 * @return array Отношения модели.
+	 */
+	public function relations()
+    {
+<?php if ($relations): ?>
+        return [
+<?php foreach($relations as $name=>$relation): ?>
+			<?php echo "'$name' => $relation,\n"; ?>
+<?php endforeach; ?>
+		];
+<?php else: ?>
+        return [];
+<?php endif; ?>
+    }
+
+	/**
+	 * @return array Атрибуты модели.
+	 */
+	public function attributeLabels()
+    {
+		return [
+<?php foreach($labels as $name=>$label): ?>
+			<?php echo "'$name' => '$label',\n"; ?>
+<?php endforeach; ?>
+		];
+	}
+
+	/**
+	 * Построение условий поиска.
+	 *
+	 * @return CActiveDataProvider Модели с применением фильтров.
+	 */
+	public function search()
+    {
+		$criteria = new CDbCriteria();
+<?php
+foreach($columns as $name=>$column)
+{
+	if($column->type==='string')
+	{
+		echo "\t\t\$criteria->compare('$name', \$this->$name, true);\n";
+	}
+	else
+	{
+		echo "\t\t\$criteria->compare('$name', \$this->$name);\n";
+	}
+}
+?>
+
+		return new CActiveDataProvider($this, ['criteria' => $criteria]);
+	}
+
+<?php if($connectionId!='db'):?>
+	/**
+	 * @return CDbConnection Класс соединения с БД.
+	 */
+	public function getDbConnection()
+    {
+		return Yii::app()-><?php echo $connectionId ?>;
+	}
+
+<?php endif?>
+	/**
+	 * Статический метод возвращения модели.
+	 *
+	 * @param string $className Имя класса.
+	 * @return <?php echo $modelClass; ?> Модель.
+	 */
+	public static function model($className = __CLASS__)
+    {
+		return parent::model($className);
+	}
+}
